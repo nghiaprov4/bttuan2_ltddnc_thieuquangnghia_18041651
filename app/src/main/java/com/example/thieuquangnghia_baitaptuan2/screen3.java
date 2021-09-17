30d0b4acb4269e890541a3f5c8e1aee6366da67b
DESKTOP-41VA80C\IBM <thieuquangnghia2000@gmail.com>
2021-09-17 15:17:35 +0700

    first commit

---
+++ app/src/main/java/com/example/thieuquangnghia_baitaptuan2/screen3.java
@@ -0,0 +1,14 @@
+package com.example.thieuquangnghia_baitaptuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen3);
+    }
+}
